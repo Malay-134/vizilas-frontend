657733525c4c2ce87d39abced3c8f9800fa3dba3
Yash Patel <148216652+yashpatel0711@users.noreply.github.com>
2026-01-05 22:35:57 +0530

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,12 +3,13 @@ import { initReactI18next } from "react-i18next";
 
 import en from "./locales/en/translation.json";
 import ln from "./locales/ln/translation.json";
-
+import ru from "./locales/ru/translation.json"; // ✅ Russian added
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
-    la: { translation: ln }
+    la: { translation: ln },
+    ru: { translation: ru } // ✅ Russian language
   },
   lng: "en", // default language
   fallbackLng: "en",
